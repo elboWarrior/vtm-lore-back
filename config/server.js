12f7c78f7d8baa14fd6cccbf36698ad87550e6e9
Camille Baillat <camille@koji.fr>
2020-09-28 08:39:05 +0200

    initial strapi configuration

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed49eb940a69ec29e1f893d84434430b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '417b6b68-d3b5-489f-9e56-1ab2b8c51506'
+};
